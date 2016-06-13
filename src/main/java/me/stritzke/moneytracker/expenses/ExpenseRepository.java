7b813bbb4d85374dd4147fd678b1231f25b233d6
Dennis Stritzke <dennis.stritzke@nttdata.com>
2016-06-13 21:00:14 +0200

    Introduced structure with day and month in endpoints. Didn't adapt frontend.

--- src/main/java/me/stritzke/moneytracker/expenses/ExpenseRepository.java
+++ src/main/java/me/stritzke/moneytracker/expenses/ExpenseRepository.java
@@ -2,9 +2,11 @@ package me.stritzke.moneytracker.expenses;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(exported = false)
 public interface ExpenseRepository extends CrudRepository<Expense, Long> {
   List<Expense> findByYearAndMonth(@Param("year") int year, @Param("month") int month);
 }
