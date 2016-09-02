afb0e278d4d8daa5c31b44b4e72453525e0d9276
Dennis Stritzke <dennis@stritzk.me>
2016-09-02 21:06:18 +0200

    #2: use correct number of decimal places everywhere

--- src/main/java/me/stritzke/moneytracker/expenses/ExpenseCreationDTO.java
+++ src/main/java/me/stritzke/moneytracker/expenses/ExpenseCreationDTO.java
@@ -3,9 +3,14 @@ package me.stritzke.moneytracker.expenses;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Data
 class ExpenseCreationDTO {
   private BigDecimal amount;
   private String comment;
+
+  void setAmount(BigDecimal amount) {
+    this.amount = amount.setScale(2, RoundingMode.CEILING);
+  }
 }
