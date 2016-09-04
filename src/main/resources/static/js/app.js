662446705d5396713135eb616ac440bd000bbd75
Dennis Stritzke <dennis@stritzk.me>
2016-09-04 19:03:11 +0200

    Fixed total counter, which accessed the wrong variable

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,14 +2,22 @@
   var moneyTracker = angular.module("moneyTracker");
 
   moneyTracker.controller("MoneyTrackerController", ["$scope", "$http", "ExpenseService", function ($scope, $http, ExpenseService) {
+    function createEmptyExpenses() {
+      return {
+        _embedded: {
+          expenses: []
+        }
+      };
+    }
+
     var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    $scope.expenses = [];
+    $scope.expenses = createEmptyExpenses();
 
     $scope.submit = createExpense;
     $scope.calculateTotal = function () {
       var total = 0;
-      for (var i = 0; i < $scope.expenses.length; i++) {
-        var expense = $scope.expenses[i];
+      for (var i = 0; i < $scope.expenses._embedded.expenses.length; i++) {
+        var expense = $scope.expenses._embedded.expenses[i];
         total = total + expense.amount;
       }
       return total;
@@ -25,24 +33,37 @@
 
     $scope.changeMonth = function (link) {
       ExpenseService.list(link).then(function (expenseResponse) {
-        $scope.expenses = expenseResponse.data;
+        setExpensesInScope(expenseResponse.data);
       }, function (err) {
         console.log("Something went wrong...");
       })
     };
 
     $scope.selectedMonthName = function () {
-      var link = $scope.expenses._links.self.href;
-      var linkComponents = link.split("/");
+      if($scope.expenses._links !== undefined) {
+        var link = $scope.expenses._links.self.href;
+        var linkComponents = link.split("/");
 
-      var year = linkComponents[linkComponents.length - 2];
-      var month = linkComponents[linkComponents.length - 1];
-      return months[month - 1] + " " + year;
+        var year = linkComponents[linkComponents.length - 2];
+        var month = linkComponents[linkComponents.length - 1];
+        return months[month - 1] + " " + year;
+      } else {
+        return "";
+      }
     };
+
+    function setExpensesInScope(expenseResource) {
+      if(expenseResource._embedded == undefined) {
+        expenseResource._embedded = {
+          expenses: []
+        };
+      }
+      $scope.expenses = expenseResource;
+    }
 
     function loadExpenses() {
       ExpenseService.listCurrent().then(function (expenseResource) {
-        $scope.expenses = expenseResource;
+        setExpensesInScope(expenseResource);
       }, function (err) {
         console.log("Something went wrong");
         console.log(err);
